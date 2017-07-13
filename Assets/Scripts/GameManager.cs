6a2a2f040062d21c956ee5e89ab58c0f25208f4a
wox0 <wox0@protonmail.com>
2017-07-13 15:18:51 +0200

    Setting up scene. Added player with fishing rod, footbridge, float, water. Added the very beginning of Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+
+    public static GameManager Instance;
+
+
+    void Awake()
+    {
+        if (Instance == null) // check if instance exists
+            Instance = this; // set instance
+        else if (Instance != this) // if instance exists and it's not this
+            Destroy(gameObject);    // destroy it
+
+        DontDestroyOnLoad(gameObject); // dont destroy this when reloading scene
+    }
+
+
+
+   
+}
